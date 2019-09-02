6bb848fd29dc20e192a811f79644a249671ce13f
Hirad Tabatabaei <54820026+hiradtaba@users.noreply.github.com>
2019-09-02 19:25:09 -0400

    Update Field.java

--- Field.java
+++ Field.java
@@ -1,14 +1,9 @@
 package Project5;
-
 public class Field extends Place{
-
 	public Field(String name, double x, double y) {
 		super(name, x, y);
 	}
-	
 	public String toString() {
 		return super.toString()+" Field";
-	}
-
-	
+	}	
 }
