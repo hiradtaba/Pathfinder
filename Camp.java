aa58d84758d03634e08a99a23423f003c7b39993
Hirad Tabatabaei <54820026+hiradtaba@users.noreply.github.com>
2019-09-02 19:23:48 -0400

    Update Camp.java

--- Camp.java
+++ Camp.java
@@ -1,11 +1,9 @@
 package Project5;
 
 public class Camp extends Accommodation {
-
 	public Camp(String name, double x, double y, int capacity) {
 		super(name, x, y, capacity);
 	}
-
 	public boolean arrive(Expedition e)
 	{
 		return (super.arrive(e) && e.getHasTents());
