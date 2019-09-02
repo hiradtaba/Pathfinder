551fa1df6b7f6724083a517d0fd3d84cb686aa6d
Hirad Tabatabaei <54820026+hiradtaba@users.noreply.github.com>
2019-09-02 19:23:19 -0400

    Update Accommodation.java

--- Accommodation.java
+++ Accommodation.java
@@ -1,18 +1,13 @@
 package Project5;
-
 public class Accommodation extends Place {
-
 	private int capacity;
-
 	public Accommodation(String name, double x, double y, int capacity) {
 		super(name, x, y);
 		this.capacity = capacity;
 	}
-
 	public int getCapacity() {
 		return capacity;
 	}	
-
 	public boolean arrive(Expedition e)
 	{
 		return (capacity >= e.getPeople()); 
