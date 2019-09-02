dc627fda480b34f3f8eaebb8c78266117bd46cf3
Hirad Tabatabaei <54820026+hiradtaba@users.noreply.github.com>
2019-09-02 17:24:32 -0400

    Update Camp.java

--- Camp.java
+++ Camp.java
@@ -1,10 +1,5 @@
 package Project5;
 
-//Name: Hirad Tabatabaei
-//Class: ECE 122
-//Instructor: Professor Marzulo
-//Due date: March 5, 2018
-
 public class Camp extends Accommodation {
 
 	public Camp(String name, double x, double y, int capacity) {
