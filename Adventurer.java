6d308525cc1cdc4236467ec779b44a6027864ed7
Hirad Tabatabaei <54820026+hiradtaba@users.noreply.github.com>
2019-09-02 17:23:28 -0400

    Update Adventurer.java

--- Adventurer.java
+++ Adventurer.java
@@ -1,10 +1,5 @@
 package Project5;
 
-//Name: Hirad Tabatabaei
-//Class: ECE 122
-//Instructor: Professor Marzulo
-//Due date: March 5, 2018
-
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
