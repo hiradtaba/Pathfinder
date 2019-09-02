1ea5e64d5d693ff0a0b0ff9744000db645b70143
Hirad Tabatabaei <54820026+hiradtaba@users.noreply.github.com>
2019-09-02 19:26:56 -0400

    Update Lodge.java

--- Lodge.java
+++ Lodge.java
@@ -1,18 +1,15 @@
 package Project5;
 
 public class Lodge extends Accommodation{
-
 	private double costPerPerson;
 	public Lodge(String name, double x, double y, int capacity, double costPerPerson) {
 		super(name, x, y, capacity);
 		this.costPerPerson = costPerPerson; 
 	}
-	
 	public double getCostPerPerson()
 	{
 		return costPerPerson;
 	}
-	
 	public boolean leave(Expedition e)
 	{
 		double lodgeCost = e.getPeople()*costPerPerson;
