e4a5e18a0babfc3babe2a2865a9acd1ef611f244
Hirad Tabatabaei <54820026+hiradtaba@users.noreply.github.com>
2019-09-08 17:45:03 -0400

    Update Place.java

--- Place.java
+++ Place.java
@@ -1,5 +1,6 @@
 package Project5;
 public abstract class Place {
+	
 	private String name;
 	private double x;
 	private double y;
@@ -17,15 +18,18 @@ public abstract class Place {
 	public double getX() {
 		return x;
 	}
+	
 	public double getY() {
 		return y;
 	}
+	
 	public double getDistance(Place destination)
 	{
 		double dx = Math.abs(x - destination.getX());
 		double dy = Math.abs(y - destination.getY());
 		return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
 	}
+	
 	public boolean goTo(Expedition e, Place destination)
 	{
 		if (leave(e))
@@ -33,10 +37,12 @@ public abstract class Place {
 				return destination.arrive(e);
 		return false;
 	}
+	
 	public boolean leave(Expedition e)
 	{
 		return true;
 	}
+	
 	public boolean arrive(Expedition e)
 	{
 		return true;
