3d4ee32b193ec3c3dcea3d0de043f9ab7d10bafb
Hirad Tabatabaei <54820026+hiradtaba@users.noreply.github.com>
2019-09-08 17:42:12 -0400

    Update Field.java

--- Field.java
+++ Field.java
@@ -3,7 +3,9 @@ public class Field extends Place{
 	public Field(String name, double x, double y) {
 		super(name, x, y);
 	}
+	
 	public String toString() {
 		return super.toString()+" Field";
 	}	
+	
 }
