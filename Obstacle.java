619a485da9d566083b03586bccc3a00308080b77
Hirad Tabatabaei <54820026+hiradtaba@users.noreply.github.com>
2019-09-08 17:43:36 -0400

    Update Obstacle.java

--- Obstacle.java
+++ Obstacle.java
@@ -1,13 +1,17 @@
 package Project5;
 public class Obstacle extends Place{
+	
 	private double ropeLength;
+	
 	public Obstacle(String name, double x, double y, double ropeLength) {
 		super(name, x, y);
 		this.ropeLength = ropeLength;
 	}
+	
 	public double getRopeLength() {
 		return ropeLength;
 	}
+	
 	public boolean arrive(Expedition e)
 	{
 		return ropeLength<=e.getRopeLength();
